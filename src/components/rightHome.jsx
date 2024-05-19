79729faf4d7867e50aebe58872c66a60f448cb6d
MGuruNikhil <gurunikhilmangaraju@gmail.com>
2024-05-19 21:24:20 +0530

    search result onClick improved

--- src/components/rightHome.jsx
+++ src/components/rightHome.jsx
@@ -25,12 +25,18 @@ const RightHome = () => {
         }
     }
 
+    const handleClick = (user)  => {
+        setSearchName("");
+        setSearchResults([]);
+        navigate("/profile/"+user.name);
+    }
+
     return (
         <div className="w-[20%] h-[100%] p-2 items-center flex flex-col space-y-2">
-            <input className="rounded-[100px] px-4 py-2 w-full bg-gray-700 focus:outline-none" type="text" name='search' id='search' placeholder='Search' onChange={handleSearchChange} onKeyDown={handleSearch} />
+            <input className="rounded-[100px] px-4 py-2 w-full bg-gray-700 focus:outline-none" type="text" name='search' id='search' placeholder='Search' onChange={handleSearchChange} onKeyDown={handleSearch} value={searchName} />
             <div className="w-full rounded-xl bg-gray-800 flex flex-col justify-center overflow-auto">
                 {searchResults && searchResults.map((user, index) => (
-                    <div key={index} onClick={()=>{navigate("/profile/"+user.name)}} className="flex gap-2 p-4 border-b-solid border-b-gray-900 border-b-[2px] cursor-pointer">
+                    <div key={index} onClick={()=>{handleClick(user)}} className="flex gap-2 p-4 border-b-solid border-b-gray-900 border-b-[2px] cursor-pointer">
                         {user.name ?(
                         <>
                             <img src={user.photo || Dummy} alt="" className="w-[1.5em] rounded-[50%]"/>
